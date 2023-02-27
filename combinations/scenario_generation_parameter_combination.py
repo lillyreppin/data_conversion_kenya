2dff1982ba4ce1a17de474169733ff6e7bccbfc0
lillyreppin <reppin.lilly@gmail.com>
2023-02-27 14:33:30 +0100

    Add capacity constraints.

--- combinations/scenario_generation_parameter_combination.py
+++ combinations/scenario_generation_parameter_combination.py
@@ -6,13 +6,15 @@ import shutil
 
 
 class Scenarios:
-    def __init__(self, path_to_cost_input: str, path_to_base_input: str, path_to_lifetime_input: str):
-        self.path_to_cost_input = path_to_cost_input
+    def __init__(self, path_to_cost_input: str, path_to_base_input: str, path_to_lifetime_input: str, path_to_capacity_input: str):
         self.path_to_base_input = path_to_base_input
+        self.path_to_capacity_input = path_to_capacity_input
+        self.path_to_cost_input = path_to_cost_input
         self.path_to_lifetime_input = path_to_lifetime_input
         # read in csv files
-        self.cost_input_data = pd.read_csv(self.path_to_cost_input)
         self.base_input_data = pd.read_csv(self.path_to_base_input)
+        self.capacity_input_data = pd.read_csv(self.path_to_capacity_input)
+        self.cost_input_data = pd.read_csv(self.path_to_cost_input)
         self.lifetime_input_data = pd.read_csv(self.path_to_lifetime_input)
 
     def scenario_generation(self, n_scenarios: int = []):
@@ -20,6 +22,7 @@ class Scenarios:
         Generate a specific number of scenarios for selected US_scenario, randomise the values for capital and fixed costs as well as lifetime.
         """
 
+        capacity_input = self.capacity_input_data.copy()
         cost_input = self.cost_input_data.copy()
         lifetime_input = self.lifetime_input_data.copy()
 
@@ -53,21 +56,21 @@ class Scenarios:
                 os.path.join(scenario_path, os.path.basename(source_path))
                 shutil.copytree(source_path, scenario_path_final, dirs_exist_ok=True)
 
-                # randomize capital costs within the min/max range
+                # randomise capital costs within the min/max range
                 cost_input["cap_random"] = random.uniform(
                     cap_min_final, cap_max_final
                 )
-                # randomize fixed costs within the min/max range
+                # randomise fixed costs within the min/max range
                 cost_input["fix_random"] = random.uniform(
                     fix_min_final, fix_max_final
                 )
 
-                # randomize lifetime
+                # randomise lifetime
                 lifetime_input["TechnicalLife_random"] = random.uniform(
                     lifetime_min, lifetime_max
                 )
 
-                # in technodata file: randomize cap_par according to cap_random
+                # in techno data file: randomise "cap_par" according to "cap_random"
                 path_techno_data = (
                     scenario_path
                     + str(num)
@@ -110,7 +113,7 @@ class Scenarios:
                     columns=["US_scenario", "cap_random", "fix_random"]
                 )
 
-                # in Technodata.csv: randomise TechnicalLife according to TechnicalLife_random
+                # in techno data file: randomise "TechnicalLife" according to "TechnicalLife_random"
                 lifetime_input = lifetime_input.astype({"Time": str})
                 technodata_final2 = pd.merge(
                     left=technodata_final,
@@ -128,21 +131,39 @@ class Scenarios:
                 )
                 technodata_final2 = technodata_final2.drop(
                     columns=["TechnicalLife_random"]
-                )               
+                )    
+                
+                # in techno data file: adapt capacity according to MaxCapacityAddition_mod and MaxCapacityGrowth_mod
+                capacity_input = capacity_input.astype({"Time": str})
+                technodata_final3 = pd.merge(
+                    left=technodata_final2,
+                    right=capacity_input[
+                        ["ProcessName", "Time", "MaxCapacityAddition_mod", "MaxCapacityGrowth_mod"]
+                    ],
+                    how="left",
+                    left_on=["ProcessName", "Time"],
+                    right_on=["ProcessName", "Time"],
+                )
+                technodata_final3["MaxCapacityAddition"] = technodata_final3["MaxCapacityAddition_mod"]
+                technodata_final3["MaxCapacityGrowth"] = technodata_final3["MaxCapacityGrowth_mod"]
+
+                technodata_final3 = technodata_final3.drop(
+                    columns=["MaxCapacityAddition_mod", "MaxCapacityGrowth_mod"]
+                )                           
 
                 os.remove(path_techno_data)
-
-                technodata_final2.to_csv(path_techno_data)
+                technodata_final3.to_csv(path_techno_data)
 
         return self
 
-
-PATH_COST_INPUT = "/../../.." # add the path
 PATH_BASE_INPUT = "/../../.." # add the path
+PATH_CAPACITY_INPUT = "/../../.." # add the path
+PATH_COST_INPUT = "/../../.." # add the path
 PATH_LIFETIME_INPUT = "/../../.." # add the path
 scenario_input = Scenarios(
-    path_to_cost_input=os.path.join(PATH_COST_INPUT, "..."), # add the file
     path_to_base_input=os.path.join(PATH_BASE_INPUT, "..."), # add the file
+    path_to_capacity_input=os.path.join(PATH_CAPACITY_INPUT, "..."), # add the file
+    path_to_cost_input=os.path.join(PATH_COST_INPUT, "..."), # add the file
     path_to_lifetime_input=os.path.join(PATH_LIFETIME_INPUT, "..."), # add the file
 )
 
